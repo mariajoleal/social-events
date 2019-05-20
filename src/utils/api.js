3c830ea60c62a5cbd2f53ee22aefbf0afafc07df
Mariajo Leal <mariajo20leal@gmail.com>
2019-05-19 18:33:24 -0600

    Connection with api

---
+++ src/utils/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export function getEvents() {
+  return axios.get('http://localhost:8080/api/events/');
+}
+
+export function addEvent(event) {
+  console.log(event);
+  return axios.post('http://localhost:8080/api/events/', event);
+} 
