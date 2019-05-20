34819597c27f62b3094769ee7b6b2eb7a0125e16
Mariajo Leal <mariajo20leal@gmail.com>
2019-05-19 18:32:19 -0600

    Made Events page

--- src/components/Events/EventsComponent.js
+++ src/components/Events/EventsComponent.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { ClipLoader } from 'react-spinners';
+import Grid from '@material-ui/core/Grid';
+import CardContainer from './components/Card/CardContainer';
+import Form from './components/Form/FormContainer';
+import './Event.css';
 
-const EventsComponent = (props) => {  
+const EventsComponent = (props) => {
+  const { loading, data, handleCreate } = props;
   return (
-    <div>Here comes the events list</div>
+    <>
+      <div className="spinner">
+      <ClipLoader
+        sizeUnit={"px"}
+        size={150}
+        color={'#123abc'}
+        loading={loading}
+      />
+      </div>      
+      {!loading && <div className="event">
+        <Grid container spacing={8}>
+          <Grid item md={3}>
+            <Form handleCreate={handleCreate} />
+          </Grid>
+          <Grid item md={9}>
+            <Grid container spacing={16}>
+              {
+                data && data.map((event) => {
+                  return (
+                    <Grid item md={4}>
+                      <CardContainer data={event} />
+                    </Grid>
+                  )
+                })
+              }
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>}
+  </>
   );
+  
 }
- 
+
 export default EventsComponent;
--- src/components/Events/EventsContainer.js
+++ src/components/Events/EventsContainer.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react'
 import EventsComponent from './EventsComponent';
+import { getEvents, addEvent } from '../../utils/api';
 
 class EventsContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // username: "",
-      // password: "",
+      events: [],
+      loading: true,
     };
 
-    // this.handleSubmit = this.handleSubmit.bind(this);
     // this.handleUsername = this.handleUsername.bind(this);
     // this.handlePass = this.handlePass.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+  }
+
+  componentDidMount() {
+    getEvents()
+      .then((res) => {
+        this.setState({
+          events: res.data.data,
+          loading: false,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  handleCreate(event) {
+    addEvent(event)
+    .then((res) => {
+      const events = this.state.events;
+      events.push(res.data.data); 
+      this.setState({
+        events,
+        loading: false,
+      });
+    })
+    .catch((err) => console.log(err));
   }
 
   render() { 
+    const { loading, events } = this.state;
+
     return (  
-      <EventsComponent />
+      <EventsComponent
+        loading={loading}
+        data={events}
+        handleCreate={this.handleCreate}
+      />
     );
   }
 }
