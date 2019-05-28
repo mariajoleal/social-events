4697b827c8f2fbd7ead7f5829f1c6dac41181186
Mariajo Leal <mariajo20leal@gmail.com>
2019-05-28 16:50:49 -0600

    Changed Login UI

--- src/components/Login/LoginComponent.js
+++ src/components/Login/LoginComponent.js
@@ -1,15 +1,112 @@
 import React from 'react';
-import Form from '../SharedComponents/Form/FormComponent';
-import './Login.css';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const LoginComponent = (props) => {
+  const classes = useStyles();
+  const { handleSubmit, handleChange, errors } = props;
+
   return (
-      <Form
-        handleSubmit={props.onSubmit}
-        onUsernameChange={props.onUsernameChange}
-        onPasswordChange={props.onPasswordChange}
-      />
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className="" onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label={errors.emailError? errors.message : "Email Address"}
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+              error={errors.emailError}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label={errors.passError? errors.message : "Password"}
+              type="password"
+              id="password"
+              onChange={handleChange}
+              error={errors.passError}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className=""
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
   );
-};
+}
 
 export default LoginComponent;
--- src/components/Login/LoginContainer.js
+++ src/components/Login/LoginContainer.js
@@ -1,41 +1,7 @@
 import React, { Component } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../utils/api';
-
-// const useStyles = makeStyles(theme => ({
-//   '@global': {
-//     body: {
-//       backgroundColor: theme.palette.common.white,
-//     },
-//   },
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   form: {
-//     width: '100%', // Fix IE 11 issue.
-//     marginTop: theme.spacing(1),
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//   },
-// }));
+import LoginComponent from './LoginComponent';
 
 class LoginContainer extends Component {
   
@@ -52,10 +18,11 @@ class LoginContainer extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
   onChange(e)  {
-    // console.log(e.target.id);
-    this.setState({ [e.target.id]: e.target.value });
-    // console.log(this.state);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   };
+
   onSubmit = e => {
     e.preventDefault();
     const userData = {
@@ -95,75 +62,14 @@ class LoginContainer extends Component {
 
   render() {
     const { success, errors } = this.state;
-    const classes = {
-      paper: {
-          marginTop: '20%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-      },
-    };
+
     return (
       success ? <Redirect to='/events' /> :
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} onSubmit={this.onSubmit} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label={errors.emailError? errors.message : "Email Address"}
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={this.onChange}
-              error={errors.emailError}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label={errors.passError? errors.message : "Password"}
-              type="password"
-              id="password"
-              onChange={this.onChange}
-              error={errors.passError}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
+      <LoginComponent 
+        errors={errors}
+        handleSubmit={this.onSubmit}
+        handleChange={this.onChange}
+      />
     );
   } 
 }
