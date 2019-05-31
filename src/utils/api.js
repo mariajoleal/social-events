1ecd436bbe8d35dab138c5fbbd61f02eb97b6864
Mariajo Leal <mariajo20leal@gmail.com>
2019-05-30 20:22:51 -0600

    Added register route

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,10 +5,13 @@ export function getEvents() {
 }
 
 export function addEvent(event) {
-  console.log(event);
   return axios.post('http://localhost:8080/api/events/', event);
 } 
 
 export function login(user) {
   return axios.post('http://localhost:8080/api/users/login', user);
+}
+
+export function register(newUser) {
+  return axios.post('http://localhost:8080/api/users/register', newUser);
 } 
